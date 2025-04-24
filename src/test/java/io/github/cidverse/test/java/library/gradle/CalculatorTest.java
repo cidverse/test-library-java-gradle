b4b735acaf60d5d223a3de2a90d568b7d199da6d
Philipp Heuer <git@philippheuer.de>
2025-04-24 19:36:42 +0200

    feat: add more methods to calculator

--- src/test/java/io/github/cidverse/test/java/library/gradle/CalculatorTest.java
+++ src/test/java/io/github/cidverse/test/java/library/gradle/CalculatorTest.java
@@ -12,4 +12,20 @@ public class CalculatorTest {
 
         assertEquals(5, result, "Expected 2 + 3 to equal 5");
     }
+
+    @Test
+    void testSubtract() {
+        Calculator calculator = new Calculator();
+        int result = calculator.subtract(5, 3);
+
+        assertEquals(2, result, "Expected 5 - 3 to equal 2");
+    }
+
+    @Test
+    void testMultiply() {
+        Calculator calculator = new Calculator();
+        int result = calculator.multiply(4, 3);
+
+        assertEquals(12, result, "Expected 4 * 3 to equal 12");
+    }
 }
